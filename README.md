5d77aff6f54dbdcd4832930b66d8143833921d6f
luiztools <luizfduartejr@gmail.com>
2021-12-05 13:32:24 -0300

    parte 1

---
+++ README.md
@@ -0,0 +1,13 @@
+# react-native-tutorials
+Simple app examples in React Native with Expo
+
+## reactnative01
+How to execute:
+
+1. cd reactnative01
+2. npm i
+3. npm start
+4. scan the QRCode with your phone
+
+Tutorial completo em https://www.luiztools.com.br/post/introducao-ao-react-native/
+
---
+++ reactnative01/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#474A51',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
